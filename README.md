49ebae221046d95038407326ccd186d62f69033f
Ellis Delgado <ellisdelgado13@gmail.com>
2020-10-24 22:41:04 -0700

    Commit 0.2 Finished Script

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Password-Generator
+
+![Password](/../Password-Generator\Password.png?raw=true"Password Generator")
+
+
+For this assignment I tried my best to take the user information given and make a randomly generated password. I while i was successful in making the password, I was unfortunately not able to get the password displayed into the text area. This challenge was a tough one, especially when dealing with the routing of if/else statements. Lucky, I was able to work with my classmates and find solutions to most of the problems I was facing.
+
+In regards to the work done on the file, the first thing I did was identify all the variables that were going to be needed for this algorithm to work. Second, I began with the while statement so that I could continue the loop while being able to validate other requests. Third, I tied the various characters to different if/then statements so that the user could choose between different options. Then, I set up more conditional statements that were based off the users selection, identifying which options had been selected, and which had not. Finally, I plugged the data into a for loop so that I could generate the random password within the given parameters. 
--- script.js
+++ script.js
@@ -1,136 +1,148 @@
 // Assignment Code
+
+
+
+// Add event listener to generate button
+
+
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+    passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", function(){
-  generatePassword();
-});
+// Write password to the #password input
+
+
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+  var specialChara = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
+  
+  var length = 0;
+  var choices ;
+
+  var lowerChoice = false;
+  var upperChoice = false;
+  var specialChoice = false;
+  var numberChoice = false;
+  
+  generateBtn.addEventListener("click", function(){
+    generatePassword();
+  });
+
+  // Beginning prompt to see how many characters the user wants in their password
 
   function generatePassword(){
-  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-  
-  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-
-  var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-
-  var specialChara = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", "\:", "\;", " < ", "=", " > ", " ? ", "@", "[", "\\", "]", " ^ ", "_", "`", "{", "|", "}", "~"];
-
-  var length = prompt("Hey my dude, how many characters do you want in your password? Choose between 8-128!");
-  
-  var options; 
-
+ 
+  length = prompt("Hey my dude, how many characters do you want in your password? Choose between 8-128!");
 
  while(length < 8 || length > 128){
     alert("Hey not those ones! Choose some different ones");
     prompt("Choose between 8-128. It ain't that hard...");
     if (length >= 8 || length <= 128){
+      console.log(length);
       break;
     }
   }
+  // validating the choices
   
   if (length >= 8 || length <= 128){
 
-    var lowerCaseInput = confirm("You want lowercase letters in your password?");
-    if (lowerCaseInput){
-      alert("Sweet!")
-    }
-    else{
-      alert("For sure then...")
-    }
-    var upperCaseInput = confirm("You want uppercase letters in your password?");
-    if (upperCaseInput){
-      alert("Sweet!")
-    }
-    else{
-      alert("Oh ok...")
-    }
-    var specialCharaInput = confirm("You want special characters in your password?");
-    if (specialCharaInput){
-      alert("Sweet!")
-    }
-    else{
-      alert("For sure then...")
-    }
-    var numCharaInput = confirm("You want numbers in your password?");
-    if (numCharaInput){
-      alert("Sweet!")
-    }
-    else{
-      alert("For sure then...")
-    }
+     lowerChoice = confirm("You want lowercase letters in your password?");
     
-    if (lowerCaseInput === false && upperCaseInput === false && specialCharaInput === false && numCharaInput === false){
-      alert("Hey! Whatta you some kinda wise guy??? You gotta pick at least one option!")
+    upperChoice = confirm("You want uppercase letters in your password?");
+    
+    specialChoice = confirm("You want special characters in your password?");
+   
+     numberChoice = confirm("You want numbers in your password?");
+
+  
+    if (lowerChoice === false && upperChoice === false && specialChoice === false && numberChoice === false){
+      choices = alert("Hey! Whatta you some kinda wise guy??? You gotta pick at least one option!")
     }
   
-    else if (lowerCaseInput){
-      options = lowerCase
+    else if (specialChoice && numberChoice && upperChoice && lowerCase){
       
+      choices = specialChara.concat(numbers, upperCase, lowerCase); 
+    }
+    //3 choices were selected by the user
+
+    else if(specialChoice && numberChoice && upperChoice){
+      choice = specialChara.concat(numbers, upperCase);
     }
     
-    else if(upperCaseInput){
-      options = upperCase
+    else if(specialChoice && numberChoice && lowerChoice){
+      choice = specialChara.concat(numbers, lowerCase);
     }
     
-    else if (numCharaInput){
-      options = numbers
+    else if(specialChoice && lowerChoice && upperChoice){
+      choice = specialChara.concat(lowerCase, upperCase);
+    }
+    else if(numberChoice && lowerChoice && upperChoice){
+      choice = numbers.concat(lowerCase, upperCase)
+    }
+    // if 2 choices were selected by the user
+
+    else if (specialChoice && numberChoice){
+      choice = specialChara.concat(numbers);
+    }
+    else if (specialChoice && lowerChoice){
+      choice = specialChara.concat(lowerCase);
+    }
+    else if (specialChoice && upperChoice){
+      choice = specialChara.concat(upperCase);
     }
     
-    else if (specialCharaInput){
-      options = specialChara
+    else if (lowerChoice && numberChoice){
+      choice = lowerCase.concat(numbers);
     }
-    else if(lowerCaseInput && upperCaseInput){
-        options = lowerCase.concat(upperCase);
+    else if (lowerChoice && upperChoice){
+      choice = lowerCase.concat(upperCase);
     }
-    else if (lowerCaseInput && specialCharaInput){
-      options = lowerCase.concat(specialChara);
+    else if (numberChoice && upperChoice){
+      choice = lowerCase.concat(upperCase);
     }
-    else if (lowerCaseInput && numCharaInput){
-      options = lowerCase.concat(numbers);
-    }
-    else if (lowerCaseInput && upperCaseInput && numCharaInput){
-      options = lowerCase.concat(upperCase, numbers);
-    }
+    // if 1 option is selected by the user
     
-    else if (upperCaseInput && specialCharaInput){
-      options = upperCase.concat(specialChara);
+    else if (specialChoice){
+      choice = specialChara;
     }
-    else if  (upperCaseInput && numCharaInput){
-      options = upperCase.concat(numbers)
-    }
-    else if (upperCaseInput && specialCharaInput && numCharaInput){
-      options = upperCase.concat(specialChara, numbers)
-    }
-    else if (specialCharaInput && numCharaInput){
-      options = specialChara.concat(numbers);
-    }
-    else if(specialCharaInput && numCharaInput && lowerCaseInput){
-      options = specialChara.concat(numbers, lowerCase);
+    else if(numberChoice){
+      choice = numbers;
     }
 
-    else if (lowerCaseInput && upperCaseInput && numCharaInput && specialCharaInput){
-      options = lowerCase.concat(upperCase, numbers, specialChara);
+    else if (lowerChoice){
+      choice = lowerCase;
     }
-
-    var password = []
-
-      for(var i = 0; i < length; i++){
-        var pickOptions= options[Math.floor(Math.random()*options.length)];
-        password.push(pickOptions)
-      }
-    
-  
-    
-    
+    else if(upperCase){
+      choice = upperCase;
+    }
     
   };
+  // inputing the data from the validations
+    var results = [];
+
+    for (var i = 0; i < length; i++){
+     var selections = choices[Math.floor(Math.random()*choices.length)];
+        results.push(selections);
+    }
+    console.log(results);
+    
+    alert("Here is your new password: " + results);
+    
+    return results.join("");
+
+   
+    
+
 };
+
+
+
+
